702c57651832fe3147f75ed4b3d9ed5b424e9744
sunliwei <sunliwei@coohua.com>
2020-08-02 11:15:26 +0800

    【update】学校列表接口版本控制

--- app/src/main/java/cn/surine/schedulex/ui/schedule_data_fetch/AdapterListRepository.kt
+++ app/src/main/java/cn/surine/schedulex/ui/schedule_data_fetch/AdapterListRepository.kt
@@ -21,7 +21,7 @@ object AdapterListRepository : BaseRepository() {
         val url: String = if (BuildConfig.DEBUG) {
             "https://surinex.coding.net/p/schedulex/d/schedulex/git/raw/dev/schools.json"
         } else {
-            "https://surinex.coding.net/p/schedulex/d/schedulex/git/raw/master/schools.json"
+            "https://surinex.coding.net/p/schedulex/d/schedulex/git/raw/master/schools_"+BuildConfig.VERSION_CODE+".json"
         }
         Loader.mService.getAdapterList(url).await()
     }
