0b2288e67b5486c155a074e500902ae0536163be
sunliwei <sunliwei@coohua.com>
2020-08-15 16:21:45 +0800

    【opt】优化课程表列表页面代码

--- app/src/main/java/cn/surine/schedulex/ui/schedule_list/ScheduleListFragment.kt
+++ app/src/main/java/cn/surine/schedulex/ui/schedule_list/ScheduleListFragment.kt
@@ -13,10 +13,7 @@ import cn.surine.schedulex.app_base.VmManager
 import cn.surine.schedulex.base.Constants
 import cn.surine.schedulex.base.controller.BaseAdapter
 import cn.surine.schedulex.base.controller.BaseFragment
-import cn.surine.schedulex.base.utils.Navigations
-import cn.surine.schedulex.base.utils.Prefs
-import cn.surine.schedulex.base.utils.Toasts
-import cn.surine.schedulex.base.utils.load
+import cn.surine.schedulex.base.utils.*
 import cn.surine.schedulex.data.entity.Schedule
 import cn.surine.schedulex.ui.course.CourseViewModel
 import cn.surine.schedulex.ui.schedule.ScheduleViewModel
@@ -49,95 +46,101 @@ class ScheduleListFragment : BaseFragment() {
     override fun layoutId() = R.layout.fragment_schedule_manager
 
     override fun onInit(parent: View?) {
-        VmManager(this).apply {
-            scheduleViewModel = vmSchedule
-            courseViewModel = vmCourse
-        }
-        data = scheduleViewModel.schedules
-        viewRecycler.load(LinearLayoutManager(activity()), BaseAdapter(data, R.layout.item_schedule_list, BR.schedule)) {
-            it.setOnItemClickListener { position ->
-                val scheduleId = data[position].roomId.toLong()
-                if (Prefs.getLong(Constants.CUR_SCHEDULE, -1L) != scheduleId) {
-                    Prefs.save(Constants.CUR_SCHEDULE, scheduleId)
-                    it.notifyDataSetChanged()
-                    Snackbar.make(addSchedule, R.string.timetable_switched_successfully, Snackbar.LENGTH_SHORT).show()
-                }
-            }
-            it.setOnItemLongClickListener { position ->
-                openScheduleSetting(position)
-                true
-            }
 
-            it.setOnItemElementClickListener(object : BaseAdapter.OnItemElementClickListener(R.id.more_function) {
-                override fun onClick(v: View?, position: Int) {
-                    with(PopupMenu(context, v)) {
-                        menuInflater.inflate(R.menu.popup_menu_schedule, menu)
-                        setOnMenuItemClickListener { item ->
-                            when (item.itemId) {
-                                R.id.popup_menu_action_1 -> openScheduleSetting(position)
-                                R.id.popup_menu_action_2 -> deleteSchedule(position)
-                                R.id.popup_menu_action_4 -> exportCourse(position)
+        init {
+            VmManager(this).apply {
+                scheduleViewModel = vmSchedule
+                courseViewModel = vmCourse
+            }
+            data = scheduleViewModel.schedules
+        }
+
+        ui {
+            viewRecycler.load(LinearLayoutManager(activity()), BaseAdapter(data, R.layout.item_schedule_list, BR.schedule)) {
+                it.setOnItemClickListener { position ->
+                    val scheduleId = data[position].roomId.toLong()
+                    if (Prefs.getLong(Constants.CUR_SCHEDULE, -1L) != scheduleId) {
+                        Prefs.save(Constants.CUR_SCHEDULE, scheduleId)
+                        it.notifyDataSetChanged()
+                        Snackbar.make(addSchedule, R.string.timetable_switched_successfully, Snackbar.LENGTH_SHORT).show()
+                    }
+                }
+                it.setOnItemLongClickListener { position ->
+                    openScheduleSetting(position)
+                    true
+                }
+
+                it.setOnItemElementClickListener(object : BaseAdapter.OnItemElementClickListener(R.id.more_function) {
+                    override fun onClick(v: View?, position: Int) {
+                        with(PopupMenu(context, v)) {
+                            menuInflater.inflate(R.menu.popup_menu_schedule, menu)
+                            setOnMenuItemClickListener { item ->
+                                when (item.itemId) {
+                                    R.id.popup_menu_action_1 -> openScheduleSetting(position)
+                                    R.id.popup_menu_action_2 -> deleteSchedule(position)
+                                    R.id.popup_menu_action_4 -> exportCourse(position)
+                                }
+                                false
                             }
-                            false
+                            show()
                         }
-                        show()
+                    }
+                })
+
+                it.setOnItemElementClickListener(object : BaseAdapter.OnItemElementClickListener((R.id.chip_config_name)) {
+                    override fun onClick(v: View?, position: Int) {
+                        openScheduleSetting(position, CHANGE_SCHEDULE_NAME)
+                    }
+                })
+
+                it.setOnItemElementClickListener(object : BaseAdapter.OnItemElementClickListener((R.id.chip_config_week)) {
+                    override fun onClick(v: View?, position: Int) {
+                        openScheduleSetting(position, CHANGE_WEEK_INFO)
+                    }
+                })
+
+                it.setOnItemElementClickListener(object : BaseAdapter.OnItemElementClickListener((R.id.chip_change_background)) {
+                    override fun onClick(v: View?, position: Int) {
+                        openScheduleSetting(position, CHANGE_BACKGROUND)
+                    }
+                })
+
+                it.setOnItemElementClickListener(object : BaseAdapter.OnItemElementClickListener((R.id.chip_course_item_height)) {
+                    override fun onClick(v: View?, position: Int) {
+                        openScheduleSetting(position, CHANGE_COURSE_ITEM_HEIGHT)
+                    }
+                })
+
+                it.setOnItemElementClickListener(object : BaseAdapter.OnItemElementClickListener((R.id.chip_more_setting)) {
+                    override fun onClick(v: View?, position: Int) {
+                        openScheduleSetting(position)
+                    }
+                })
+            }
+            topbar.setFunctionIcon(R.drawable.ic_settings_black_24dp)
+        }
+
+
+        click {
+            viewRecycler.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+                override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                    super.onScrolled(recyclerView, dx, dy)
+                    addSchedule.apply {
+                        if (dy > 0) shrink() else extend()
                     }
                 }
             })
-
-            it.setOnItemElementClickListener(object : BaseAdapter.OnItemElementClickListener((R.id.chip_config_name)) {
-                override fun onClick(v: View?, position: Int) {
-                    openScheduleSetting(position, CHANGE_SCHEDULE_NAME)
-                }
-            })
-
-            it.setOnItemElementClickListener(object : BaseAdapter.OnItemElementClickListener((R.id.chip_config_week)) {
-                override fun onClick(v: View?, position: Int) {
-                    openScheduleSetting(position, CHANGE_WEEK_INFO)
-                }
-            })
-
-            it.setOnItemElementClickListener(object : BaseAdapter.OnItemElementClickListener((R.id.chip_change_background)) {
-                override fun onClick(v: View?, position: Int) {
-                    openScheduleSetting(position, CHANGE_BACKGROUND)
-                }
-            })
-
-            it.setOnItemElementClickListener(object : BaseAdapter.OnItemElementClickListener((R.id.chip_course_item_height)) {
-                override fun onClick(v: View?, position: Int) {
-                    openScheduleSetting(position, CHANGE_COURSE_ITEM_HEIGHT)
-                }
-            })
-
-            it.setOnItemElementClickListener(object : BaseAdapter.OnItemElementClickListener((R.id.chip_more_setting)) {
-                override fun onClick(v: View?, position: Int) {
-                    openScheduleSetting(position)
-                }
-            })
-        }
-        viewRecycler.addOnScrollListener(object : RecyclerView.OnScrollListener() {
-            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
-                super.onScrolled(recyclerView, dx, dy)
-                addSchedule.apply {
-                    if (dy > 0) shrink() else extend()
+            topbar.getFunctionView().setOnClickListener {
+                Navigations.open(this, R.id.action_ScheduleListFragment_to_aboutFragment)
+            }
+            addSchedule.setOnClickListener {
+                if (scheduleViewModel.schedulesNumber < Constants.MAX_SCHEDULE_LIMIT) {
+                    Navigations.open(this, R.id.action_ScheduleListFragment_to_scheduleInitFragment)
+                } else {
+                    Toasts.toast(getString(R.string.no_permission_to_add))
                 }
             }
-        })
-
-        topbar.setFunctionIcon(R.drawable.ic_settings_black_24dp)
-        topbar.getFunctionView().setOnClickListener {
-            Navigations.open(this, R.id.action_ScheduleListFragment_to_aboutFragment)
         }
-
-
-        addSchedule.setOnClickListener {
-            if (scheduleViewModel.schedulesNumber < Constants.MAX_SCHEDULE_LIMIT) {
-                Navigations.open(this, R.id.action_ScheduleListFragment_to_scheduleInitFragment)
-            } else {
-                Toasts.toast(getString(R.string.no_permission_to_add))
-            }
-        }
-
     }
 
     private fun openScheduleSetting(position: Int, tag: Int? = null) {
